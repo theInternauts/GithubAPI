9254847feb64bd63e27ea8696f26cbb000b2fb00
theInternauts <cricketw@wearetheinternauts.com>
2021-07-28 22:47:14 +0900

    add my usual light debug and layout utils

---
+++ GithubAPI/Bootstrap/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  GithubAPI
+//
+//  Created by Christopher Wallace on 7/28/21.
+//
+
+import Foundation
+
+enum GithubAPIApp {
+    // DEBUGGING UTILS
+    static let debugUtilsEnabled = true
+}
--- GithubAPI/Modules/Views/RepoSearchViewController.swift
+++ GithubAPI/Modules/Views/RepoSearchViewController.swift
@@ -18,3 +18,57 @@ class RepoSearchViewController: UIViewController {
 extension RepoSearchViewController: RepoSearchPresenterToViewProtocol {
     
 }
+
+
+// MARK - Debugging Utils
+extension UIViewController {
+    // DEBUGGING UTILS
+    static var debugUtilsEnabled = GithubAPIApp.debugUtilsEnabled
+    
+    static func printUtil(_ msgs: [String: Any]) {
+        if debugUtilsEnabled {
+            for (label,msg) in msgs {
+                print("\(label): \(msg)")
+            }
+        }
+    }
+}
+
+
+// MARK: - Layout Utilities
+extension UIView {
+    func pinToEdges(of superview: UIView, constrainToMargins: Bool = false) {
+        /*
+            Use cases cover edge-to-edge fullscreen (ignoring the notch/safeAreas) AND
+            full page margin-to-margin on all four sides (may or may not respect safeAreas)
+            based on margin constraints set in the containing view.
+         
+            NOTE TO SELF: CHECK WITH THE TECH LEAD about margin conventions before
+            going wild with this in production
+        */
+        var rules: [NSLayoutConstraint]?
+        translatesAutoresizingMaskIntoConstraints = false
+        if constrainToMargins {
+            rules = [
+                topAnchor.constraint(equalTo: superview.layoutMarginsGuide.topAnchor),
+                leadingAnchor.constraint(equalTo: superview.layoutMarginsGuide.leadingAnchor),
+                trailingAnchor.constraint(equalTo: superview.layoutMarginsGuide.trailingAnchor),
+                bottomAnchor.constraint(equalTo: superview.layoutMarginsGuide.bottomAnchor)
+            ]
+        } else {
+            rules = [
+                topAnchor.constraint(equalTo: superview.topAnchor),
+                leadingAnchor.constraint(equalTo: superview.leadingAnchor),
+                trailingAnchor.constraint(equalTo: superview.trailingAnchor),
+                bottomAnchor.constraint(equalTo: superview.bottomAnchor)
+            ]
+        }
+        NSLayoutConstraint.activate(rules!)
+    }
+    
+    func addSubviews(_ views: UIView...) {
+        for view in views {
+            addSubview(view)
+        }
+    }
+}
